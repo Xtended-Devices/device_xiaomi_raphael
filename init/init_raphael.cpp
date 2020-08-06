97a15fe28983adc62034466fcbb1d3b67e269b4c
Akash <akashniki@gmail.com>
2020-08-06 23:14:47 +0200

    raphael: bump to september coral build fp

--- init/init_raphael.cpp
+++ init/init_raphael.cpp
@@ -89,8 +89,8 @@ void vendor_load_properties()
     }
 
     // fingerprint
-    property_override("ro.build.description", "raphael-user 10 QKQ1.190825.002 V11.0.8.0 release-keys");
-    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys");
+    property_override("ro.build.description", "coral-user 11 RP1A.200720.009 6720564 release-keys");
+    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/coral/coral:11/RP1A.200720.009/6720564:user/release-keys");
 
     load_dalvikvm_properties();
 }
